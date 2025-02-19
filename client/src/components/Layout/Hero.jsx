72ddabb683442ad0f8d7a148e7c6d2f5dd38bfe0
jabastinKEC <jabastina.24mca@kongu.edu>
2025-02-19 16:29:44 +0530

    Feat: Basic features of header section is added

--- client/src/components/Layout/Hero.jsx
+++ client/src/components/Layout/Hero.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import heroImg from "../../assets/rabbit-hero.webp";
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
-  )
-}
+    <section>
+      <img
+        src={heroImg}
+        alt="Image about brand clothings"
+        className="w-full h-[400px] md:h-[600px] lg:h-[750] object-cover"
+      />
+      <div className="absolute inset-0 bg-black flex items-center justify-center">
+        <div className="text-center text-white p-6">
+            <h1 className="text-4xl md:text-9xl font-bold tracking-tighter uppercase mb-4">
+                Vacation
+            </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
