7a186f264782db4b85764d603a3990e4fb21ab92
JabastinKEC <jabastina.24mca@kongu.edu>
2025-02-20 16:10:55 +0530

    Feat: New Arrivals section is on progress

--- client/src/components/Pages/Home.jsx
+++ client/src/components/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from '../Layout/Hero'
 import GendersCollections from '../Products/GendersCollections'
+import NewArrivals from '../Products/NewArrivals'
 const Home = () => {
   return (
     <div>
       <Hero/>
       <GendersCollections/>
+      <NewArrivals/>
     </div>
   )
 }
