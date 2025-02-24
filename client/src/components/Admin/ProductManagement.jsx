c779adbf7a1bed21329b5c93fd935af3359e5f4f
JABASTIN <itsmejabastin@gmail.com>
2025-02-24 21:11:22 +0530

    completed: Product-management is fully styled

--- client/src/components/Admin/ProductManagement.jsx
+++ client/src/components/Admin/ProductManagement.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const ProductManagement = () => {
   const products = [
     {
@@ -7,6 +9,12 @@ const ProductManagement = () => {
       sku: "123123123",
     },
   ];
+
+  const handleClick = (id) => {
+    if (window.confirm("Are you sure you want to delete the product?")) {
+      console.log("Delete Product with ID:", id);
+    }
+  };
   return (
     <div className="max-w-7xl mx-auto p-6">
       <h2 className="text-2xl font-bold mb-6">Product Management</h2>
@@ -30,10 +38,31 @@ const ProductManagement = () => {
                   <td className="p-4 font-medium text-gray-900 whitespace-nowrap ">
                     {product.name}
                   </td>
+                  <td className="p-4">${product.price}</td>
+                  <td className="p-4">${product.sku}</td>
+                  <td className="p-4">
+                    <Link
+                      to={`/admin/products/${product._id}/edit`}
+                      className=" bg-yellow-500 text-[#fff] px-2 py-1.5 rounded mr-2 hover:bg-yellow-600
+                    "
+                    >
+                      Edit
+                    </Link>
+                    <button
+                      onClick={() => handleClick(product._id)}
+                      className="bg-red-500 text-[#fff] px-2 py-1 rounded hover:bg-red-600"
+                    >
+                      Delete
+                    </button>
+                  </td>
                 </tr>
               ))
             ) : (
-              <tr></tr>
+              <tr>
+                <td colSpan={4} className="p-4 text-center text-gray-500">
+                  No Product Found
+                </td>
+              </tr>
             )}
           </tbody>
         </table>
