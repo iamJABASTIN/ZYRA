37519887a8f7dbd6abcec7a2719ec2e9455b3bb2
iamJABASTIN <itsmejabastin@gmail.com>
2025-02-26 15:11:59 +0530

    Filter logic and sorting logic added

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -133,22 +133,119 @@ router.put("/:id", protect, admin, async (req, res) => {
 // @desc Delete a product by ID
 // @access Private/admin
 
-router.delete("/:id", protect, admin, async ( req, res) => {
-    try {
-        //Find the product by ID
-        const product = await Product.findById(req.params.id);
+router.delete("/:id", protect, admin, async (req, res) => {
+  try {
+    //Find the product by ID
+    const product = await Product.findById(req.params.id);
 
-        if(product) {
-            // Remove the product from DB
-            await product.deleteOne();
-            res.json({message: "Product removed"});
-        } else {
-            res.status(404).json({message: "Product not found"});
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Server Error");
+    if (product) {
+      // Remove the product from DB
+      await product.deleteOne();
+      res.json({ message: "Product removed" });
+    } else {
+      res.status(404).json({ message: "Product not found" });
     }
-})
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
 
+// @route GET /api/products
+// @desc Get all products with optional query filters
+// @access Public
+
+router.get("/", async (req, res) => {
+  try {
+    // Get all products from DB
+    const {
+      collection,
+      size,
+      color,
+      gender,
+      minPrice,
+      maxPrice,
+      sortBy,
+      search,
+      category,
+      material,
+      brand,
+      limit,
+    } = req.query;
+
+    let query = {};
+
+    //Filter logic
+    if (collection && collection.toLocaleLowerCase() !== "all") {
+      query.collections = collection;
+    }
+
+    if (category && category.toLocaleLowerCase() !== "all") {
+      query.category = category;
+    }
+
+    if (material) {
+      query.material = { $in: material.split(",") };
+    }
+
+    if (brand) {
+      query.brand = { $in: brand.split(",") };
+    }
+
+    if (size) {
+      query.sizes = { $in: size.split(",") };
+    }
+
+    if (color) {
+      query.colors = { $in: [color] };
+    }
+
+    if (gender) {
+      query.gender = gender;
+    }
+
+    if (minPrice || maxPrice) {
+      query.price = {};
+      if (minPrice) {
+        query.price.gte = Number(minPrice);
+      }
+      if (maxPrice) {
+        query.price.lte = Number(maxPrice);
+      }
+    }
+
+    if (search) {
+      query.$or = [
+        { name: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } },
+      ];
+    }
+
+    // Sort Logic
+    let sort = {};
+    if(sortBy) {
+      switch(sortBy) {
+        case "priceAsc":
+          sort() = { price: 1 };
+          break;
+        case "priceDesc":
+          sort() = { price: -1 };
+          break;
+        case "popularity":
+          sort() = { rating: -1 };
+          break;
+        default:
+          break;
+      }
+    }
+
+    //Fetch products and apply sorting and limit
+
+    let products = await Product.find(query).sort(sort).limit(Number(limit) || 0);
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
---
+++ server/seeder.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Product = require("./models/Product");
+const User = require("./models/User");
+const products = require("./data/products");
+
+dotenv.config();
+
+//Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI);
+
+//Function to seed data
+const seedData = async () => {
+  try {
+    // Clear a default admin User
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    // Create a new admin User
+    const createdUser = await User.create({
+      name: "Admin User",
+      email: "admin@example.com",
+      password: "123456",
+      role: "admin",
+    });
+
+    //Assign the default user ID to each product
+
+    const userID = createdUser._id;
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: userID };
+    });
+
+    //Insert the sample products to the database
+    await Product.insertMany(sampleProducts);
+
+    console.log("Product data Seeded Successfully");
+    process.exit();
+  } catch (error) {
+    console.error("Error seeding the data:",error);
+    process.exit(1);
+  }
+};
+
+seedData();
