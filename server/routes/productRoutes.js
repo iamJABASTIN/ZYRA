905610eb8576122857fc14165965c26b64412d0a
iamJABASTIN <itsmejabastin@gmail.com>
2025-02-26 10:51:49 +0530

    Create request for product is completed

---
+++ server/routes/productRoutes.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const Product = require("../models/Product");
+const { protect, admin } = require("../middleware/authMiddleware");
+const router = express.Router();
+
+// @route POST /api/products
+// @desc Create a new Product
+// @acces Private/Admin
+
+router.post("/", protect, admin, async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      category,
+      brand,
+      sizes,
+      colors,
+      collections,
+      materials,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+      sku,
+    } = req.body;
+
+    const product = new Product({
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      category,
+      brand,
+      sizes,
+      colors,
+      collections,
+      materials,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+      sku,
+      user: req.user._id,
+    });
+
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require("express")
 const cors = require("cors")
 const dotenv = require("dotenv")
 const connectDB = require("./config/db")
-const useRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes")
+const productRoutes = require("./routes/productRoutes")
 
 const app = express();
 app.use(express.json());
@@ -20,7 +21,8 @@ app.get("/", (req, res) => {
 });
 
 //API Routes
-app.use("/api/users", useRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/products", productRoutes)
 
 
 app.listen(PORT, () => {
