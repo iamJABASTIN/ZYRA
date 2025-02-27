a9138c3dab814a8db403f47f90a287cac338c84a
iamJABASTIN <itsmejabastin@gmail.com>
2025-02-27 15:27:43 +0530

    DELETE -route added to the cart

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -100,4 +100,85 @@ router.post("/", async (req, res) => {
   }
 });
 
+// @route PUT /api/cart
+// @desc Update product quantity in the cart for a guest or logged-in user
+// @access Public
+
+router.put("/", async (req, res) => {
+  const { productId, quantity, size, color, guestId, userId } = req.body;
+
+  try {
+    let cart = await getCart(userId, guestId);
+    if (!cart) return res.status(404).json({ message: "Cart not found" });
+
+    const productIndex = cart.products.findIndex(
+      (p) =>
+        p.productId.toString() === productId &&
+        p.size === size &&
+        p.color === color
+    );
+
+    if (productIndex > -1) {
+      // Update the quantity of the product
+      if (quantity > 0) {
+        cart.products[productIndex].quantity = parseInt(quantity);
+      } else {
+        cart.products.splice(productIndex, 1); // Remove the product if quantity is 0
+      }
+
+      cart.totalPrice = cart.products.reduce(
+        (acc, item) =>
+          acc + (parseFloat(item.price) || 0) * (item.quantity || 1),
+        0
+      );
+      await cart.save();
+
+      return res.status(200).json(cart);
+    } else {
+      return res.status(404).json({ message: "Product not found in cart" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+// @route DELETE /api/cart
+// @desc REMOVE a product from the cart
+// @access Public
+
+router.delete("/", async (req, res) => {
+  const { productId, size, color, guestId, userId } = req.body;
+
+  try {
+    let cart = await getCart(userId, guestId);
+    if (!cart) return res.status(404).json({ message: "Cart not found" });
+
+    const productIndex = cart.products.findIndex(
+      (p) =>
+        p.productId.toString() === productId &&
+        p.size === size &&
+        p.color === color
+    );
+
+    if (productIndex > -1) {
+      cart.products.splice(productIndex, 1);
+
+      cart.totalPrice = cart.products.reduce(
+        (acc, item) =>
+          acc + (parseFloat(item.price) || 0) * (item.quantity || 1),
+        0
+      );
+      await cart.save();
+
+      return res.status(200).json(cart);
+    } else {
+      return res.status(404).json({ message: "Product not found in cart" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server Error" });
+  }
+});
+
 module.exports = router;
