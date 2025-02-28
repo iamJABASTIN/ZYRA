5f7ed6256c42b8b41eefa741edb4b8f0c74d4071
iamJABASTIN <itsmejabastin@gmail.com>
2025-02-28 12:16:06 +0530

    UPLOAD and SUBSCRIBE -Route reques are added

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const checkoutRoutes = require("./routes/checkoutRoutes");
 const orderRoutes = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
+const subscribeRoute = require("./routes/subscribeRoute");
+
 
 const app = express();
 app.use(express.json());
@@ -29,6 +32,8 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api", subscribeRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
